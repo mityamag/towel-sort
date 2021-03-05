f014fca2410c1bbd578438f90bcf15c1ede050fb
mityamag <78642476+mityamag@users.noreply.github.com>
2021-03-05 09:43:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+return matrix.filter((item, index) => index % 2 !== 0 ? item.reverse() : item);
 }
